ba9b2f82b8394a1b2880c1ce9aba9ea3b1adb6ce
Jose Francisco <onefran2004@gmail.com>
2025-12-19 00:18:48 -0600

    se colocaron los nombres de los integrantes del equipo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:segimutiplataform/src/routes/AppRoutes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+/*
+EQUIPO:
+
+Jose Francisco Hernandez Fernando
+Jesus Orlando Garrido Cruz
+Alan Sanchez Garrido
+Guadalupe Cruz Hernandez
+
+ */
+
 Future<void> main() async{
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
